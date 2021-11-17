272a558bda54209d8acb986383648604429b1efe
Crazy365 <Crazy365@users.noreply.github.com>
2021-11-17 19:07:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Reset commit
-git reset --hard f598cbf18dec856a6bf3a08490ed74d1003c960c
+#git reset --hard f598cbf18dec856a6bf3a08490ed74d1003c960c
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
