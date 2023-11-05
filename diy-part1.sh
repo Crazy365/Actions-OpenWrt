c0b914dd54ab07b736f7fc37993b8435f6bf60b1
Crazy365 <Crazy365@users.noreply.github.com>
2023-11-05 15:23:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld;main' feeds.conf.default
